e57d1eeed6db14d853eb9836eb245e029b5fd91c
officialshreeya <shreeyalve@gmail.com>
2025-06-20 06:35:28 +0000

    [automatic commit] 2025-06-20 06:35:28 UTC 14.201.75.40

--- part2/app.js
+++ part2/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const session = require('express-session');
+const mysql = require('mysql2/promise');
 
 const app = express();
 
