95e6afcf06e0e8be04f15b75a7a0e7921885e5a9
officialshreeya <shreeyalve@gmail.com>
2025-06-20 09:14:48 +0000

    [automatic commit] 2025-06-20 09:14:48 UTC 120.20.202.208

--- part2/app.js
+++ part2/app.js
@@ -3,16 +3,12 @@ const path = require('path');
 require('dotenv').config();
 const session = require('express-session');
 
-
+const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(session({
-  secret: 'secret123',
-  resave: false,
-  saveUninitialized: true,
-}));
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
