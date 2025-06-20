8f05d588fc3a7a62235a6228c1b30358824aeca4
officialshreeya <shreeyalve@gmail.com>
2025-06-20 08:19:30 +0000

    [automatic commit] 2025-06-20 08:19:29 UTC 14.201.75.40

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -33,7 +33,6 @@ router.get('/me', (req, res) => {
   }
   res.json(req.session.user);
 });
-
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
