4c5f22bc4074ce55fbd127d8588b14f4741a9ded
officialshreeya <shreeyalve@gmail.com>
2025-06-20 09:51:42 +0000

    [automatic commit] 2025-06-20 09:51:42 UTC 120.20.202.208

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,6 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // Save session
     req.session.user = rows[0];
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
