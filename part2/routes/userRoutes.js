f246fbc6db753b64fe5f0803721250921b969cee
officialshreeya <shreeyalve@gmail.com>
2025-06-20 14:51:09 +0000

    [automatic commit] 2025-06-20 14:51:08 UTC 14.201.75.40

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -71,7 +71,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      return res.status(500).json({ error: 'Logout failed' });
+      return res.status(500).json({ error: 'logout failed' });
     }
 
     res.clearCookie('connect.sid');
