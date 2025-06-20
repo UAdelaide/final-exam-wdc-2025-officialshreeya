03a4291e665fe2269a7c216a598698e1b4e12308
officialshreeya <shreeyalve@gmail.com>
2025-06-20 06:40:57 +0000

    [automatic commit] 2025-06-20 06:40:56 UTC 14.201.75.40

--- part2/app.js
+++ part2/app.js
@@ -43,12 +43,5 @@ app.use(session({
     console.error('Error connecting to DB:', err);
   }
 })();
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
-
 // Export the app instead of listening here
 module.exports = app;
