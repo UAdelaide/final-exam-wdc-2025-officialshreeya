8da75ffde81e47fd79025822b871b2f69e6c5496
officialshreeya <shreeyalve@gmail.com>
2025-06-20 09:13:36 +0000

    [automatic commit] 2025-06-20 09:13:35 UTC 120.20.202.208

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,19 +37,19 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+      WHERE username = ? AND password_hash = ?
+    `, [username, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // Save to session
+    // Save session
     req.session.user = rows[0];
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
